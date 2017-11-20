e912790817cc5a3da2e1428780872124114f0232
Jordan PALLARD <jpallard@e3r6p8.42.fr>
2017-11-20 12:09:09 +0100

    travailler sur .h qui a votre prenom

---
+++ include/judi.h
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   asm.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdaufin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/18 19:15:51 by jdaufin           #+#    #+#             */
+/*   Updated: 2017/11/18 19:20:22 by jdaufin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ASM_H
+# define ASM_H
+
+# include "op.h"
+# include "libft.h"
+# include <fcntl.h>
+
+# define HASH			'#'
+# define SPACE			' ' 
+# define TAB			'\t'
+# define DOT			'.'
+# define COMMA			','
+# define COTE			'\"'
+# define COLON			':'
+
+typedef struct			s_info
+{
+	char				name[129];
+	char				comment[2047];
+}						t_info;
+
+/*
+**	The following structure stores, among other things:
+**	- a series of label strings associated w/ the given instruction
+**	- an array (ty_param) of the signatures of the parameters
+**	- an int (pos) to store the Byte index of the instruction
+*/
+
+typedef struct			s_order
+{
+	char				**label;
+	char				**param;
+	int					ty_param[3];
+	int					op_code;
+	int					nb_param;
+	int					pos; 
+}						t_order;
+
+typedef struct			s_file
+{
+	char				*line;
+	int					fd;
+	int					ret;
+}						t_file;
+
+#endif
