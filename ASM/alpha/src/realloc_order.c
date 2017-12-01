8778ebc83bc57dba814e76d505d879e532a25277
Jean-Judicael DAUFIN <jdaufin@student.42.fr>
2017-12-01 14:47:42 +0100

    realloc_order fixed

--- ASM/alpha/src/realloc_order.c
+++ ASM/alpha/src/realloc_order.c
@@ -6,7 +6,7 @@
 /*   By: jgonthie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 11:23:11 by jgonthie          #+#    #+#             */
-/*   Updated: 2017/12/01 11:24:24 by jgonthie         ###   ########.fr       */
+/*   Updated: 2017/12/01 14:46:37 by jdaufin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void		realloc_order(t_order ****order, int *size)
 {
 	*size += 1;
-	if ((*order = realloc(*order, sizeof(t_order *) *
+	if ((**order = realloc(**order, sizeof(t_order *) *
 					(*size * SIZE_STRUCT))) == NULL)
 		error("[ERR] : Ft. Realloc Failed\n");
 }
