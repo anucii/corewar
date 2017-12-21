0c041c18f47da8cbbe88d0b4bf9e4c80dfdc9044
Jordan PALLARD <jpallard@student.42.fr>
2017-12-21 16:21:51 +0100

    fix sti and remove a printf

--- VM/src_jordan/int_on_mem.c
+++ VM/src_jordan/int_on_mem.c
@@ -6,7 +6,7 @@
 /*   By: jpallard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/15 16:48:15 by jpallard          #+#    #+#             */
-/*   Updated: 2017/12/16 16:45:29 by jpallard         ###   ########.fr       */
+/*   Updated: 2017/12/21 14:25:04 by jpallard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ void	int_on_mem(unsigned char *mem, unsigned int i, unsigned short s)
 	i1 = i >> 24;
 	i2 = i >> 16;
 	i3 = i >> 8;
-	ft_printf("i = %u, i1 = %hhu, i2 = %hhu, i3 = %hhu\n ",i, i1, i2, i3);
 	mem[s % MEM_SIZE] = i1;
 	mem[(s + 1) % MEM_SIZE] = i2;
 	mem[(s + 2) % MEM_SIZE] = i3;
