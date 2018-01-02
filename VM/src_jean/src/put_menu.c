81dedd596041d1d2ec49cbe7562898ebcedc80aa
Jordan PALLARD <jpallard@student.42.fr>
2018-01-02 17:53:19 +0100

    bgm done

--- VM/src_jean/src/put_menu.c
+++ VM/src_jean/src/put_menu.c
@@ -6,13 +6,13 @@
 /*   By: jgonthie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/15 15:00:06 by jgonthie          #+#    #+#             */
-/*   Updated: 2017/12/30 16:29:27 by jpallard         ###   ########.fr       */
+/*   Updated: 2018/01/02 17:51:31 by jpallard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-static void	print_corewar(t_info *info)
+static	void	print_corewar(t_info *info)
 {
 	mvwprintw(info->win, (HEIGHT_WIN / 2) - 12, 75, " ________  ________  ________  _______   ___       __   ________  ________     ");
 	mvwprintw(info->win, (HEIGHT_WIN / 2) - 11, 75, "|\\   ____\\|\\   __  \\|\\   __  \\|\\  ___ \\ |\\  \\     |\\  \\|\\   __  \\|\\   __  \\    ");
@@ -23,7 +23,51 @@ static void	print_corewar(t_info *info)
 	mvwprintw(info->win, (HEIGHT_WIN / 2) - 6, 75, "    \\|_______|\\|_______|\\|__|\\|__|\\|_______|\\|____________|\\|__|\\|__|\\|__|\\|__|");
 }
 
-static void	draw_menu(t_info *info)
+static	void	choice_of_music(FMOD_CHANNELGROUP *canal, FMOD_SYSTEM *system,
+		FMOD_SOUND *bgm[4], int i)
+{
+	FMOD_BOOL state;
+
+	FMOD_ChannelGroup_GetPaused(canal, &state);
+	if (state == false )
+	{
+		FMOD_ChannelGroup_Stop(canal);
+		FMOD_Sound_SetLoopCount(bgm[i], -1);
+		FMOD_System_PlaySound(system, bgm[i], 0, false, NULL);
+	}
+	else
+	{
+		FMOD_Sound_SetLoopCount(bgm[i], -1);
+		FMOD_System_PlaySound(system, bgm[i], 0, false, NULL);
+	}
+}
+
+static void		manage_music(FMOD_SYSTEM *s, FMOD_SOUND *bgm[4], t_info *info)
+{
+	FMOD_CHANNELGROUP *c;
+	char p;
+
+	FMOD_System_GetMasterChannelGroup(s, &c);
+	while (1)
+	{
+		p = wgetch(info->win);
+		if (p == 'p')
+			FMOD_ChannelGroup_Stop(c);
+		else if (p == ' ')
+		{
+			FMOD_System_PlaySound(s, bgm[3], 0, false, NULL);
+			break ;
+		}
+		else if (p == '1')
+			choice_of_music(c, s, bgm, 0);
+		else if (p == '2')
+			choice_of_music(c, s, bgm, 1);
+		else if (p == '3')
+			choice_of_music(c, s, bgm, 2);
+	}
+}
+
+static	void	draw_menu(t_info *info, FMOD_SYSTEM *s, FMOD_SOUND *bgm[4])
 {
 	wattron(info->win, COLOR_PAIR(3));
 	print_corewar(info);
@@ -31,31 +75,44 @@ static void	draw_menu(t_info *info)
 	mvwprintw(info->win, (HEIGHT_WIN / 2) + 3, 106, "PRESS SPACE TO BEGIN");
 	wattroff(info->win, A_UNDERLINE);
 	mvwprintw(info->win, 70, 10, "By : JDaufin");
+	mvwprintw(info->win, 68, 200, "Choose a bgm song with 1, 2, 3");
 	mvwprintw(info->win, 72, 10, "     JPallard");
+	mvwprintw(info->win, 70, 200, "1 = choosen by jdaufin ");
 	mvwprintw(info->win, 74, 10, "     JGonthie");
+	mvwprintw(info->win, 72, 200, "2 = choosen by jpallard");
+	mvwprintw(info->win, 74, 200, "3 = choosen by jgonthie");
 	wrefresh(info->win);
-	while (getch() != ' ')
-		;
+	manage_music(s, bgm, info);
 	wattroff(info->win, COLOR_PAIR(3));
 }
 
 void		put_menu(t_info *info)
 {
-	FMOD_SYSTEM *system;
-	FMOD_SOUND *bgm;
-	FMOD_RESULT res;
+	FMOD_SYSTEM *s;
+	FMOD_SOUND *bgm[4];
+	FMOD_RESULT res[4];
+	int			i = 0;;
 
-	FMOD_System_Create(&system);
-	FMOD_System_Init(system, 1, FMOD_INIT_NORMAL, NULL);
-	res = FMOD_System_CreateSound(system, "./include/AR.mp3", FMOD_LOOP_NORMAL, 0, &bgm);
-	if (res != FMOD_OK)
+	i = 0;
+	FMOD_System_Create(&s);
+	FMOD_System_Init(s, 5, FMOD_INIT_NORMAL, NULL);
+	res[0] = FMOD_System_CreateSound(s, "./include/AR.mp3", FMOD_LOOP_NORMAL, 0, &bgm[0]);
+	res[1] = FMOD_System_CreateSound(s, "./include/Tetris.mp3", FMOD_LOOP_NORMAL, 0, &bgm[1]);
+//	res[2] = FMOD_System_CreateSound(s, "...", FMOD_LOOP_NORMAL, 0, &bgm[2]);
+	res[3] = FMOD_System_CreateSound(s, "./include/ready.ogg", FMOD_CREATESAMPLE, 0, &bgm[3]);
+	while (i < 4)
 	{
-		ft_printf("FUCCCCCCCCCCCCCCCCCCK\n");
-		exit(EXIT_FAILURE);
+		if (res[i] != FMOD_OK)
+		{
+			destroy_win(info);
+			exit(EXIT_FAILURE);
+		}
+		i++;
 	}
-	FMOD_Sound_SetLoopCount(bgm, -1);
-	FMOD_System_PlaySound(system, bgm, 0, false, NULL);
 	new_win(info, MENU);
-	draw_menu(info);
+	draw_menu(info, s, bgm);
 	destroy_win(info);
+//	FMOD_Sound_Release(bgm);
+//	FMOD_System_Close(system);
+//	FMOD_System_Release(system);
 }
